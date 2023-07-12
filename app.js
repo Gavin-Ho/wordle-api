31d9111473a01b84d2aac1882c52e0c923dbd0ce
Gavin <gavinjsho@gmail.com>
2023-07-11 17:20:14 -0700

    Update standings

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 // Read and parse the JSON file
-const data = fs.readFileSync(path.join(__dirname, '2023-07-09.json')); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const data = fs.readFileSync(path.join(__dirname, '2023-07-10.json')); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const json = JSON.parse(data);
 
 // Create an object to store the participants and all their wordle scores
@@ -78,7 +78,7 @@ function calculateScore(startDate, endDate) {
 
 
 // Create an object to store the monthly scores for each participant
-const julyScores = calculateScore(742, 750) // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const julyScores = calculateScore(742, 751) // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const juneScores = calculateScore(712, 725);
 const mayScores = calculateScore(681, 694);
 const aprilScores = calculateScore(651, 664);
@@ -127,7 +127,7 @@ const hallOfFame = [
     }
 ];
 
-const updateDate = "09"; // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const updateDate = "10"; // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 app.get('/api', (req, res) => {
     res.send(masterOutput);
